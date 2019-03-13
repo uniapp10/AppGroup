3f049852a418fcd2ea0eb11d9f6737d7f3ef785b
zhudongdong <zhudongdong@jujin8.com>
2019-03-13 15:27:53 +0800

    添加工程

---
+++ appOne/appOne/ViewController.m
@@ -0,0 +1,69 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by ZD on 2019/1/18.
+//  Copyright © 2019 ZD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *pwdTF;
+
+@property (nonatomic, strong) NSUserDefaults *groupDefault;
+
+@property (nonatomic, copy) NSString *keyString;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.groupDefault = [[NSUserDefaults alloc] initWithSuiteName:@"group.com.zhudong10.AppSuite"];
+    self.keyString = @"group.com.zhudong10.pwd";
+    
+}
+- (IBAction)saveAction:(id)sender {
+    
+    NSString *pwdString = self.pwdTF.text;
+    if (!pwdString.length) {
+        [self showTipString:@"pwd can't be empty"];
+        return;
+    }
+    
+    [self.groupDefault setObject:pwdString forKey:self.keyString];
+    [self showTipString:@"pwd save success"];
+    
+}
+
+- (void)showTipString:(NSString *)tips{
+    
+    UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"Tips" message:tips preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"ok" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+        [alertC popoverPresentationController];
+    }];
+    [alertC addAction:action];
+    [self presentViewController:alertC animated:true completion:nil];
+    
+}
+
+- (IBAction)clearAction:(id)sender {
+    
+     self.pwdTF.text = nil;
+    
+}
+
+- (IBAction)readAction:(id)sender {
+    
+    NSString *pwdString = [self.groupDefault objectForKey:self.keyString];
+    self.pwdTF.text = pwdString;
+    
+}
+
+@end
+
---
+++ appTwo/appTwo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  appTwo
+//
+//  Created by ZD on 2019/3/7.
+//  Copyright © 2019 ZD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
